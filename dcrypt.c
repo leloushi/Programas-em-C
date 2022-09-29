40088e34d3d47175b0fa1bf8291d0d6dc661fa97
llch0 <114681369+llch0@users.noreply.github.com>
2022-09-29 20:31:50 -0300

    Add files via upload

---
+++ dcrypt.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+     int main(){
+         char intputFile[50];
+         char outputFile[50];
+         char ch;
+         int numAscii;
+
+
+         FILE *arquivo, *arquivo2;
+         printf("\n Digite o nome do arquivo que será descripitado: ");
+         scanf("%s", intputFile);
+
+         printf("\n Digite o nome da descriptografia: ");
+         scanf("%s", outputFile);
+
+
+
+         arquivo=fopen(intputFile,"r");
+         if(arquivo==NULL){
+            printf("\n Esse texto não existe...\n");
+            exit(0);
+         }
+         else{
+            arquivo2=fopen(outputFile, "w");
+            ch=fgetc(arquivo);
+            while(ch!=EOF){
+                numAscii=(int)ch;
+                numAscii=numAscii-5;
+                fprintf(arquivo2,"%c",numAscii);
+                ch=fgetc(arquivo);
+            }
+            fclose(arquivo2);
+         }
+         fclose(arquivo);
+         printf("\n Arquivo descriptado com o nome %s.", outputFile);
+         return 0;
+}
